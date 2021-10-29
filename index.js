c7b77b0a61c278e79c5e3af77ea7e949b7adabd4
Kirill <freshjs.ftd@gmail.com>
2021-10-29 14:55:40 +0300

    Add createTable

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const {Client} = require('pg');
-const {loadUsers} = require('./api');
+const { Client } = require('pg');
+const { loadUsers } = require('./api');
 const config = require('./configs/db.json');
 
 const client = new Client(config);
 
 async function start() {
   await client.connect();
+
+  await client.query(`CREATE TABLE IF NOT EXISTS "users"(
+    id serial PRIMARY KEY,
+    first_name varchar(64) NOT NULL CHECK(first_name != ''),
+    last_name varchar(64) NOT NULL CHECK(last_name != ''),
+    email varchar(256) NOT NULL UNIQUE,
+    birthday date NOT NULL,
+    height numeric (3, 2) CHECK (
+      height > 0.2
+      AND height < 3
+    ),
+    is_male boolean,
+    CONSTRAINT "CHECK_EMAIL_NOT_EMPTY" CHECK (email != '')
+  );`);
 
   const users = await loadUsers();
   const result = await client.query(`
@@ -20,15 +34,16 @@ async function start() {
     ${mapUsers(users)};
   `);
 
-
   await client.end();
 }
 
 function mapUsers(usersArr) {
   return usersArr
     .map(
-      ({gender, name: {first, last}, email, dob: {date} }) =>
-        `('${first}', '${last}', '${email}', '${date}', ${Math.random() + 1}, ${gender === "male"})`
+      ({ gender, name: { first, last }, email, dob: { date } }) =>
+        `('${first}', '${last}', '${email}', '${date}', ${Math.random() + 1}, ${
+          gender === 'male'
+        })`
     )
     .join(',');
 }
